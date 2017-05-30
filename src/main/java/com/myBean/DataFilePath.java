26a34780c78a730a10b9ee821694b21269bc9158
bingo <2994143266@qq.com>
2017-05-30 21:01:38 +0800

    change path

--- src/main/java/com/myBean/DataFilePath.java
+++ src/main/java/com/myBean/DataFilePath.java
@@ -2,7 +2,7 @@ package com.myBean;
 
 public class DataFilePath {
 	
-	private final static String filePath = "F:\\jsp\\project\\addressBook Maven Webapp\\src\\main\\resources\\data.txt";
+	private final static String filePath = "F:\\jsp\\project\\addressBook_Maven\\src\\main\\resources\\data.txt";
 	
 	public static String getFilePath() {
 		return filePath;
